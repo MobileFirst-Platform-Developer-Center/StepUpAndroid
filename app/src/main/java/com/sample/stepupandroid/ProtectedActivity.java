c15b84e3e1bf4935c340bf4b5e5263b853235a86
Shmulik Bardosh <SHMULIKB@il.ibm.com>
2016-03-20 16:57:20 +0200

    dev #4

--- app/src/main/java/com/sample/stepupandroid/ProtectedActivity.java
+++ app/src/main/java/com/sample/stepupandroid/ProtectedActivity.java
@@ -1,5 +1,7 @@
 package com.sample.stepupandroid;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +11,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by shmulikb on 14/03/16.
  */
 public class ProtectedActivity extends AppCompatActivity {
+    private ProtectedActivity _this;
     private TextView helloTextView, errorMsgTextView;
     private Button getBalanceButton, transferFundsButton, logoutButton;
 
@@ -23,18 +29,26 @@ public class ProtectedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(DEBUG_NAME, "onCreate");
 
+        _this = this;
         setContentView(R.layout.activity_protected);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar_with_logout_button);
 
         //Initialize the UI elements
-        helloTextView = (TextView)findViewById(R.id.HelloTextView);
+        helloTextView = (TextView)findViewById(R.id.helloTextView);
         getBalanceButton = (Button)findViewById(R.id.getBalance);
         transferFundsButton = (Button)findViewById(R.id.transferFunds);
         logoutButton = (Button)findViewById(R.id.logout);
         errorMsgTextView = (TextView)findViewById(R.id.errorMsg);
-        helloTextView.setText("Hello User");
-        errorMsgTextView.setText("Errors here...");
+
+        //Show the display name
+        try {
+            SharedPreferences preferences = _this.getSharedPreferences(Constants.PREFERENCES_FILE, Context.MODE_PRIVATE);
+            JSONObject user = new JSONObject(preferences.getString(Constants.PREFERENCES_KEY_USER,null));
+            helloTextView.setText("Hello " + user.getString("displayName"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         getBalanceButton.setOnClickListener(new View.OnClickListener() {
             @Override
